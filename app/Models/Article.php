69218cb74c97dfbb8cdc3d7b361566cb3ef39777
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2023-05-23 23:01:04 +0700

    build: add article model

---
+++ app/Models/Article.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Article extends Model
+{
+    protected $fillable = [
+        'author_id',
+        'category_id',
+        'title',
+        'slug',
+        'body',
+        'is_published',
+        'published_at',
+    ];
+
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'author_id');
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
